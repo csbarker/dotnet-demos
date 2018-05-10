0693e52618e3949ca1b0eb1a163740e66ce8db3f
Callum Barker <callum.scott.barker@gmail.com>
2018-05-10 21:34:39 +0800

    quickstarts

---
+++ classes-demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using classes;
+
+namespace classes_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Callum", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance");
+
+            account.MakeWithdrawl(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // TEST: Initial deposits must be positive
+            try
+            {
+                var invalid_account = new BankAccount("Invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            // TEST: Balances must not be able to go into negative
+            try
+            {
+                account.MakeWithdrawl(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
---
+++ collections-demo/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collections_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkingWithStrings();
+            WorkingWithFibonnaciNumbers();
+        }
+
+        public static void WorkingWithFibonnaciNumbers()
+        {
+            var FibonacciNumbers = new List<int> { 1, 1 };
+
+            while (FibonacciNumbers.Count < 20)
+            {
+                var previous = FibonacciNumbers[FibonacciNumbers.Count - 1];
+                var previous2 = FibonacciNumbers[FibonacciNumbers.Count - 2];
+                FibonacciNumbers.Add(previous + previous2);
+            }
+
+            Console.WriteLine(FibonacciNumbers.Count);
+            Console.WriteLine(FibonacciNumbers[0]);
+            Console.WriteLine(FibonacciNumbers[1]);
+            Console.WriteLine(FibonacciNumbers[FibonacciNumbers.Count - 1]);
+        }
+
+        public static void WorkingWithStrings() {
+            var names = new List<string> { "Callum", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine();
+
+            // modifying a list dynamically
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            // using an index
+            Console.WriteLine($"My name is {names[0]}");
+
+            // counting
+            Console.WriteLine($"The list has {names.Count} names in it");
+
+            // searching
+            Console.WriteLine();
+
+            var indexes = new List<int> {
+                names.IndexOf("Felipe"),
+                names.IndexOf("NotFound")
+            };
+
+            foreach (var index in indexes)
+	        {
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an index is not found it returns {index}");
+                } else
+                {
+                    Console.WriteLine($"The name of {names[index]} is at index {index}");
+                }
+	        }
+
+            // sorting
+            Console.WriteLine();
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}");
+            }
+        }
+    }
+}
